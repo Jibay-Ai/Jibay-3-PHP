4acfc14b28476e1cb237947ade640d05cb73129c
Jibay <jibay.co@gmail.com>
2025-08-10 12:39:08 +0330

    Jibay-3.php

---
+++ Jibay-3.php
@@ -0,0 +1,29 @@
+<?php
+
+// Created By Jibay
+// Model is Jibay-3.0
+
+$key = 'YOUR_API_KEY';
+$text = 'Hi Jibay , How are you?';
+
+$url = 'https://piki-h.ir/Jibay/j-3.php';
+
+$postData = [
+    'key' => $key,
+    'text' => $text
+];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo 'curl error: ' . curl_error($ch);
+} else {
+    echo $response;
+}
+
+curl_close($ch);
+?>
